e4d5bd30b125ea4525e0c06e20998f5f2f3111d7
Shourya Kumar <shouryakumar0301@gmail.com>
2026-07-15 21:24:40 +0530

    edited

--- sender.cpp
+++ sender.cpp
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -10,11 +11,20 @@ using namespace std;
 
 int main() {
     int recv_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (recv_sock < 0) return 1;
+    
+    int opt = 1;
+    setsockopt(recv_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     struct sockaddr_in source_addr = {0};
     source_addr.sin_family = AF_INET;
     source_addr.sin_port = htons(47010);
     source_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    bind(recv_sock, (struct sockaddr *)&source_addr, sizeof(source_addr));
+    
+    if (::bind(recv_sock, (struct sockaddr *)&source_addr, sizeof(source_addr)) < 0) {
+        perror("sender failed to bind port 47010");
+        return 1;
+    }
 
     int send_sock = socket(AF_INET, SOCK_DGRAM, 0);
     struct sockaddr_in relay_addr = {0};
@@ -29,27 +39,35 @@ int main() {
         ssize_t bytes_read = recvfrom(recv_sock, buffer, sizeof(buffer), 0, nullptr, nullptr);
         if (bytes_read < 164) continue;
 
-        uint32_t current_seq = ntohl(*(uint32_t*)buffer);
+        uint32_t current_seq;
+        memcpy(&current_seq, buffer, 4);
+        current_seq = ntohl(current_seq);
+
         frame_history[current_seq] = vector<uint8_t>(buffer + 4, buffer + 164);
 
+        bool send_fec = (current_seq % 10 != 0);
+        int pkt_size = send_fec ? 324 : 164;
+        
         unsigned char outgoing_packet[324];
         memcpy(outgoing_packet, buffer, 164); 
 
-        for (int i = 0; i < 160; i++) {
-            uint8_t prev1 = 0;
-            uint8_t prev3 = 0;
-            
-            if (current_seq >= 1 && frame_history.count(current_seq - 1)) {
-                prev1 = frame_history[current_seq - 1][i];
+        if (send_fec) {
+            for (int i = 0; i < 160; i++) {
+                uint8_t prev1 = 0;
+                uint8_t prev3 = 0;
+                
+                if (current_seq >= 1 && frame_history.count(current_seq - 1)) {
+                    prev1 = frame_history[current_seq - 1][i];
+                }
+                if (current_seq >= 3 && frame_history.count(current_seq - 3)) {
+                    prev3 = frame_history[current_seq - 3][i];
+                }
+                
+                outgoing_packet[164 + i] = prev1 ^ prev3;
             }
-            if (current_seq >= 3 && frame_history.count(current_seq - 3)) {
-                prev3 = frame_history[current_seq - 3][i];
-            }
-            
-            outgoing_packet[164 + i] = prev1 ^ prev3;
         }
 
-        sendto(send_sock, outgoing_packet, 324, 0, (struct sockaddr *)&relay_addr, sizeof(relay_addr));
+        sendto(send_sock, outgoing_packet, pkt_size, 0, (struct sockaddr *)&relay_addr, sizeof(relay_addr));
 
         if (current_seq >= 2000) {
             frame_history.erase(current_seq - 2000);
